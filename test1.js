070fb5101346c46a4502fa638a16a79680c40073
SreeChaitnaa <89938357+SreeChaitnaa@users.noreply.github.com>
2021-09-02 00:23:49 +0530

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,23 @@
+function add_product(name, count){
+    var settings = {
+  "url": "https://naturals-d1c4.restdb.io/rest/inventory",
+  "method": "POST",
+  "timeout": 0,
+  "headers": {
+    "x-apikey": "612f97f843cedb6d1f97eba5",
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "prod_name": name,
+    "count": count
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+}
+
+console.log("Test JS Loaded")
+add_product("Product100", 100)
+console.log("Test JS Called")
