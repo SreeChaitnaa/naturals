a6131ed9e19900fda6f13686b71233c97899378f
SreeChaitnaa <89938357+SreeChaitnaa@users.noreply.github.com>
2021-09-02 03:33:49 +0530

    Update test4.js

--- test4.js
+++ test4.js
@@ -20,18 +20,20 @@ $.ajax(settings).done(function (response) {
 
 console.log("Test JS Loaded")
 
-function page_loaded(){
-  if($("button")[0].innerText == "Login"){
-      $("#username")[0].value="KA0020"; 
-      $("#password")[0].value = "jaisriram123"; 
-      $("button")[0].click();
-  }
+if($("button")[0].innerText == "Login"){
+  $("#username")[0].value="KA0020"; 
+  $("#password")[0].value = "jaisriram123"; 
+  $("button")[0].click();
+}
 
-  if($("div.modal-title")[0].innerText == "CheckList"){
-      z=$x("/html/.//div/div[2]/label[1]/input"); 
-      for(i=0; i < z.length; i++){
-          z[i].checked = true
-      }
-      CheckListSubmit(this);
+if($("div.modal-title")[0].innerText == "CheckList"){
+  z=$x("/html/.//div/div[2]/label[1]/input"); 
+  for(i=0; i < z.length; i++){
+      z[i].checked = true
   }
+  CheckListSubmit(this);
+}
+
+if($("#AppointMethod")[0].options.length == 3){
+    $("#AppointMethod")[0].options.remove(2)
 }
