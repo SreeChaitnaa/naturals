b309cd6b4cc50ac96f9b78d4eb1a2bbe4b6524a6
SreeChaitnaa <89938357+SreeChaitnaa@users.noreply.github.com>
2021-10-14 19:52:54 +0530

    Update sca.js

--- sca.js
+++ sca.js
@@ -45,7 +45,7 @@ if (window.location.href.startsWith("https://iservenaturals.in")) {
     {
         loadCSS('https://sreechaitnaa.github.io/naturals/sca.css')
         loadingDiv = document.createElement('div')
-        $('#page-content-wrapper')[0].appendChild(loadingDiv)
+        document.body.appendChild(loadingDiv)
         loadingDiv.outerHTML = '<div id="divloadingscreen" class="divLoading" style="display:none"><div class="Panel-Loading-BG"></div><div id="Panel-Loading"><div></div></div></div>'
     }
     $('#divloadingscreen').show()
