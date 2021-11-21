312c2bcb03f3b15a04aac218c4419f6704414a59
SreeChaitnaa <89938357+SreeChaitnaa@users.noreply.github.com>
2021-11-21 09:33:25 +0530

    Update sca.js

--- sca.js
+++ sca.js
@@ -897,7 +897,7 @@ function doMMDBill(InvoiceModels){
             return
         }
         
-        numerator = 10
+        numerator = 4
         denominator = 10
         rand_value = Number(Math.random() * 100).toFixed() % denominator
 
