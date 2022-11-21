e10015bc77b61acd59b9f0d623d51819d5b0c9f2
SreeChaitnaa <89938357+SreeChaitnaa@users.noreply.github.com>
2022-11-21 20:16:45 +0530

    Update sca.js
    change whatsapp offer text

--- sca.js
+++ sca.js
@@ -126,7 +126,7 @@ AppointmentMessage = "Thanks for contacting Naturals Thanisandra!%0a%0a*Appointm
                      "Location: https://bit.ly/naturals-blr"
 
 
-SalesMessage = "*NEVER MISS OUR OFFER, Save this mobile number* %0a%0a" +
+SalesMessage = "*Save this number to NEVER MISS A DEAL...* %0a%0a" +
                "Thank you for using our services @ Naturals Thanisandra!%0a" +
                "Please share your review here https://bit.ly/rate-naturals %0a%0a" +
                "*Appointments:* +91 8861 567 550 %0a" +
