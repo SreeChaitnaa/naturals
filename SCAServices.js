e308a567d5ef3bef91486e860f78b90f96e26515
SreeChaitnaa <89938357+SreeChaitnaa@users.noreply.github.com>
2023-01-19 21:15:06 +0530

    Create SCAServices.js

---
+++ SCAServices.js
@@ -0,0 +1,187 @@
+SCAServices = [
+    {
+        "ServiceCode": "100001",
+        "ServiceName": "MEN COMBO PACKAGE 1 1480 @ 1299",
+        "actualPrice": 1299
+    },
+    {
+        "ServiceName": "MEN COMBO PACKAGE 2 2180 @ 1799",
+        "actualPrice": 1799,
+        "ServiceCode": "100002"
+    },
+    {
+        "ServiceName": "MEN COMBO PACKAGE 3 2930 @ 2399",
+        "actualPrice": 2399,
+        "ServiceCode": "100003"
+    },
+    {
+        "ServiceName": "MEN COMBO PACKAGE 4 3300 @ 2699",
+        "actualPrice": 2699,
+        "ServiceCode": "100004"
+    },
+    {
+        "ServiceName": "MEN COMBO All in One 4580 @ 3599",
+        "actualPrice": 3599,
+        "ServiceCode": "100005"
+    },
+    {
+        "ServiceName": "WOMEN COMBO PACKAGE 1 3585 @ 2999",
+        "actualPrice": 2999,
+        "ServiceCode": "100006"
+    },
+    {
+        "ServiceName": "WOMEN COMBO PACKAGE 2 4185 @ 3499",
+        "actualPrice": 3499,
+        "ServiceCode": "100007"
+    },
+    {
+        "ServiceName": "WOMEN COMBO PACKAGE 3 4785 @ 3999",
+        "actualPrice": 3999,
+        "ServiceCode": "100008"
+    },
+    {
+        "ServiceName": "WOMEN COMBO PACKAGE 4 5485 @ 4499",
+        "actualPrice": 4499,
+        "ServiceCode": "100009"
+    },
+    {
+        "ServiceName": "STARTER PACKAGE - GOLD - 12965 @ 10999",
+        "actualPrice": 10999,
+        "ServiceCode": "100010"
+    },
+    {
+        "ServiceName": "STARTER PACKAGE - SILVER - 10285 @ 8999",
+        "actualPrice": 8999,
+        "ServiceCode": "100011"
+    },
+    {
+        "ServiceName": "LUXURY BRIDAL PACKAGE - 27430 @ 21999",
+        "actualPrice": 21999,
+        "ServiceCode": "100012"
+    },
+    {
+        "ServiceName": "PREMIUM BRIDAL PACKAGE - 22950 @ 18999",
+        "actualPrice": 18999,
+        "ServiceCode": "100013"
+    },
+    {
+        "ServiceName": "POPULAR BRIDAL PACKAGE - 17750 @ 14899",
+        "actualPrice": 14899,
+        "ServiceCode": "100014"
+    },
+    {
+        "ServiceName": "PARTY MAKEUP - KRYLON",
+        "actualPrice": 2999,
+        "ServiceCode": "100015"
+    },
+    {
+        "ServiceName": "PARTY MAKEUP - MAC",
+        "actualPrice": 4999,
+        "ServiceCode": "100016"
+    },
+    {
+        "ServiceName": "BRIDAL MAKEUP - KRYLON",
+        "actualPrice": 4499,
+        "ServiceCode": "100017"
+    },
+    {
+        "ServiceName": "BRIDAL MAKEUP - MAC",
+        "actualPrice": 7499,
+        "ServiceCode": "100018"
+    },
+    {
+        "ServiceName": "HAIR DO - BASIC",
+        "actualPrice": 999,
+        "ServiceCode": "100019"
+    },
+    {
+        "ServiceName": "HAIR DO - CREATIVE",
+        "actualPrice": 1799,
+        "ServiceCode": "100020"
+    },
+    {
+        "ServiceName": "SAREE DRAPING",
+        "actualPrice": 499,
+        "ServiceCode": "100021"
+    },
+    {
+        "ServiceName": "BRIDAL SAREE DRAPING",
+        "actualPrice": 649,
+        "ServiceCode": "100022"
+    },
+    {
+        "ServiceName": "PARTY MAKEOVER - KRYLON - 4622 @ 4199",
+        "actualPrice": 4199,
+        "ServiceCode": "100023"
+    },
+    {
+        "ServiceName": "PARTY MAKEOVER - MAC - 6622 @ 5699",
+        "actualPrice": 5699,
+        "ServiceCode": "100024"
+    },
+    {
+        "ServiceName": "BRIDAL MAKEOVER - KRYLON - 7072 @ 6199",
+        "actualPrice": 6199,
+        "ServiceCode": "100025"
+    },
+    {
+        "ServiceName": "BRIDAL MAKEOVER - MAC - 10072 @ 8999",
+        "actualPrice": 8999,
+        "ServiceCode": "100026"
+    },
+    {
+        "ServiceName": "TRIAL MAKEUP - KRYLON",
+        "actualPrice": 1099,
+        "ServiceCode": "100027"
+    },
+    {
+        "ServiceName": "TRIAL MAKEUP - MAC",
+        "actualPrice": 1699,
+        "ServiceCode": "100028"
+    },
+    {
+        "ServiceName": "TRIAL HAIR DO - Max 2 triALs",
+        "actualPrice": 999,
+        "ServiceCode": "100029"
+    },
+    {
+        "ServiceName": "MEHENDI - ARABIC",
+        "actualPrice": 499,
+        "ServiceCode": "100030"
+    },
+    {
+        "ServiceName": "MEHENDI - HAND",
+        "actualPrice": 799,
+        "ServiceCode": "100031"
+    },
+    {
+        "ServiceName": "MEHENDI - LEG",
+        "actualPrice": 599,
+        "ServiceCode": "100032"
+    },
+    {
+        "ServiceName": "MEHENDI - BRIDAL SPECIAL",
+        "actualPrice": 1599,
+        "ServiceCode": "100033"
+    },
+    {
+        "ServiceName": "Rebonding - L",
+        "actualPrice": 10000,
+        "ServiceCode": "300001"
+    },
+    {
+        "ServiceName": "Rebonding - M",
+        "actualPrice": 8000,
+        "ServiceCode": "300002"
+    },
+    {
+        "ServiceName": "ROOTDEEP ANTI HAIRLOSS TREATMENT",
+        "ServiceCode": "400001",
+        "actualPrice": 2500
+    },
+    {
+        "ServiceName": "DANDRUFF CONTROL TREATMENT",
+        "ServiceCode": "400002",
+        "actualPrice": 2200
+    }
+]
