4c0fda321fa62498973bc55ed578b93b6cc46209
SreeChaitnaa <naturals.thanisandra@gmail.com>
2023-09-06 20:11:35 +0530

    reporting

--- LSASCA/js/reports.js
+++ LSASCA/js/reports.js
@@ -1,6 +1,18 @@
 ReportOptions = {
-    "EmployeeSalesReport":"Employee Sales Report",
-    "Invoices":"Invoices"
+    "Employee Sales Report": get_invoice_table_row,
+    "Invoices": get_invoice_table_row,
+    "Service Report": get_service_report_row
+}
+
+function employee_name(emp_id){
+    emp_map = { "700946": "Raghu", "700947": "Mary", "700948": "Suresh",
+                "700949": "Margarate", "700950": "Meenakshi", "700951": "Muskan",
+                "700952": "Suprita", "700953": "Shekar", "700954": "Nagaraju"}
+    emp_id = emp_id.toString()
+    if(emp_map[emp_id] != undefined){
+        return emp_map[emp_id]
+    }
+    return "Emp-"+emp_id
 }
 
 function from_date(){
@@ -8,7 +20,7 @@ function from_date(){
 }
 
 function to_date(){
-    $('#todt')[0].value.replace("-","").replace("-","")
+    return $('#todt')[0].value.replace("-","").replace("-","")
 }
 
 function selected_option(){
@@ -18,28 +30,85 @@ function selected_option(){
 function LoadReports(){
     for(opt in ReportOptions){
         var option = document.createElement("option");
-        option.text = ReportOptions[opt];
+        option.text = opt;
         $('#reportopt')[0].add(option);
     }
     $('#fromdt')[0].valueAsDate = new Date()
     $('#todt')[0].valueAsDate = new Date()
 }
 
+function get_invoice_table_row(bill, return_columns=false){
+    if(return_columns){
+        column_names = []
+        column_names.push("Bill No")
+        column_names.push("Amount")
+        return column_names
+    }
+    row_data = {}
+    row_data["Bill No"] = bill.Ticket_Details[0].TicketID
+    row_data["Amount"] = bill.Ticket_Details[0].Subtotal
+    return [row_data]
+}
+
+function get_service_report_row(bill, return_columns=false){
+    if(return_columns){
+        column_names = []
+        column_names.push("Bill No")
+        column_names.push("Service Name")
+        column_names.push("Employee Name")
+        column_names.push("Price")
+        column_names.push("Qty")
+        column_names.push("Mem Discount")
+        column_names.push("Other Discount")
+        column_names.push("Total Discount")
+        column_names.push("Net Sale")
+        return column_names
+    }
+    services = []
+    bill.Ticket_Product_Details.forEach(function(service, index, array){
+        row_data = {}
+        row_data["Bill No"] = bill.Ticket_Details[0].TicketID
+        row_data["Service Name"] = service.Descr
+        row_data["Employee Name"] = employee_name(service.EmpID)
+        row_data["Price"] = service.Retail_Price
+        row_data["Qty"] = service.Qty
+        row_data["Mem Discount"] = service.Mem_Disc
+        row_data["Other Discount"] = service.Oth_Disc
+        row_data["Total Discount"] = service.Discount_Amt
+        row_data["Net Sale"] = service.Total/1.18
+        services.push(row_data)
+    })
+    return services
+}
+
 function show_reports(){
+    initiate_db()
     min_date = from_date()
     max_date = to_date()
     get_rest_data_by_date(max_date, min_date, function(err, res){
         column_names = []
+        table_rows = []
         first_row = true
+        method = ReportOptions[selected_option()]
         res.forEach(function(value, index, array) {
             bill = JSON.parse(value.bill_data)
-            
+            if(first_row)
+                column_names = method(null, first_row)
+            first_row = false
+            table_row = method(bill)
+            console.log(table_row)
+            if(Array.isArray(table_row))
+                table_rows = table_rows.concat(table_row)
+            else
+                table_rows.push(table_row)
         })
+        set_table_data(column_names, table_rows)
     })
 
 }
 function set_table_data(cols, jsonData){
     table = $('#reporttbl')[0]
+    table.innerHTML = ""
 
     // Create the header element
     thead = document.createElement("thead");
