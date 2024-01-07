60926970c40e86f971c7774eeec51d70e60ea29e
SreeChaitnaa <89938357+SreeChaitnaa@users.noreply.github.com>
2024-01-07 12:05:37 +0530

    Use other DB

--- LSASCA/RestDB.py
+++ LSASCA/RestDB.py
@@ -5,11 +5,11 @@ from LSASCA.Utils import *
 
 class RestDB(object):
     def __init__(self):
-        self.url = "https://lasnrs-c22b.restdb.io/rest/"
-        x_apikey = "8329f05b6a3b424ee1e476d79740cd0816fc8"
+        # self.url = "https://lasnrs-c22b.restdb.io/rest/"
+        # x_apikey = "8329f05b6a3b424ee1e476d79740cd0816fc8"
         
-        # self.url = "https://scanrstsd1-560a.restdb.io/rest/"
-        # x_apikey = '63c50bd5969f06502871af1d'
+        self.url = "https://scanrstsd1-560a.restdb.io/rest/"
+        x_apikey = '63c50bd5969f06502871af1d'
         
         self.headers = {'content-type': "application/json",
                         'x-apikey': x_apikey,
