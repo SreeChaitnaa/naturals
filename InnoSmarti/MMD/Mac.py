0b16ea8ee2646a1def9c91ec5e894b47a6233b34
SreeChaitnaa <89938357+SreeChaitnaa@users.noreply.github.com>
2024-12-10 23:27:13 +0530

    Update Mac.py

--- InnoSmarti/MMD/Mac.py
+++ InnoSmarti/MMD/Mac.py
@@ -5,5 +5,5 @@ from Launcher import Launcher
 logging.getLogger().setLevel(logging.INFO)
 
 if __name__ == '__main__':
-    Launcher(True, True, True).process()
+    Launcher(True, True, True, "1526").process()
 
