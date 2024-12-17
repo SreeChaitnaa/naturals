6f7f8a6c7c18c958ffefe3630aabb41d42a332ad
SreeChaitnaa <89938357+SreeChaitnaa@users.noreply.github.com>
2024-12-17 18:34:57 +0530

    Update Reports.js

--- InnoSmarti/js/Reports.js
+++ InnoSmarti/js/Reports.js
@@ -4,6 +4,7 @@ table_columns = {
     "DayWise" : ["Date", "Bills", "Discount", "Net Sale", "Tax", "Gross"],
     "EmpSale": ["FirstName", "TicketCount", "TotalServiceCount", "NetSalesForServices", "ProductSales", "MembershipCardSales"]
 }
+
 function show_reports()
 {
     const myHeaders = new Headers();
@@ -49,19 +50,33 @@ function format_data(data, reportType)
     switch(reportType)
     {
         case "DayWise":
+        case "DayWiseSplit":
+        case "DayWiseNRSOnly":
             data["bills"].forEach(bill => {
+				add_bill = true
+				is_mmd = bill["TicketID"].toString().startsWith("MMD") 
                 bill_date = bill["Created_Date"].split(" ")[0]
+                if("DayWiseSplit" == reportType){
+                    if(is_mmd){
+                        bill_date = bill_date + "-SCA"
+                    }
+                }
+                if("DayWiseNRSOnly" == reportType){
+                    add_bill = !is_mmd
+                }
                 if(!(bill_date in response))
                 {
                     response[bill_date] = {}
                     table_columns["DayWise"].forEach(tk => {response[bill_date][tk] = 0})
                     response[bill_date]["Date"] = bill_date
                 }
-                response[bill_date]["Bills"] += 1
-                response[bill_date]["Discount"] += bill["Discount"]
-                response[bill_date]["Net Sale"] += bill["Total"]
-                response[bill_date]["Tax"] += bill["Tax"]
-                response[bill_date]["Gross"] += bill["Gross"]
+				if(add_bill){
+					response[bill_date]["Bills"] += 1
+					response[bill_date]["Discount"] += bill["Discount"]
+					response[bill_date]["Net Sale"] += bill["Total"]
+					response[bill_date]["Tax"] += bill["Tax"]
+					response[bill_date]["Gross"] += bill["Gross"]
+				}
             })
             response = Object.values(response).sort((a, b) => a.Date - b.Date);
             break
