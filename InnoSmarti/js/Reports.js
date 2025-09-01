ee174d991b52d3789c586e1c85a16c22c7a88b4e
Murali <muralidhar.maram@komprise.com>
2025-09-02 02:27:24 +0530

    no message

--- InnoSmarti/js/Reports.js
+++ InnoSmarti/js/Reports.js
@@ -2,12 +2,24 @@
 
 // ==== SHOP CONFIG (paste encrypted keys from Python output) ====
 const shopConfig = {
-  "TNS": { "url": "https://tns.marammuralidhar3234.workers.dev/rest/",
-    "encKey": "ki62s6ktea89p72RjFNDDrqVjKrUUT9i7d78cup3kqCEK1KvuFaLDkY7PdjUrnT5S5eQzFF2/9FGDGrt3SflYA==" },
-  "JKR": { "url": "https://jkr.marammuralidhar3234.workers.dev/rest/",
-    "encKey": "Fr4R+Qu6i1p+hdfcmDrm4bywTHY/b863GlNnuA9p9uo3x5nnahlJqvssPOJRt1BcDXLw34a2rIrcT81DmMe7xQ==" },
-  "HRLR": { "url": "https://hrlr.marammuralidhar3234.workers.dev/rest/",
-    "encKey": "bKuxUJfSNeOtp67m3v4WC39P+xPn/7a5QJNCYF1ogTW0OXSgOaxpNb+jvSBQq+93kZGHU3Bj87QFi9FAk7No+g==" }
+  "TNS": {
+    "url": "https://tns.marammuralidhar3234.workers.dev/rest/",
+    "encKey": "ki62s6ktea89p72RjFNDDrqVjKrUUT9i7d78cup3kqCEK1KvuFaLDkY7PdjUrnT5S5eQzFF2/9FGDGrt3SflYA==",
+    "name": "Thanisandra",
+    "whatsapp_number": ["918722036021", "917829347636"]
+  },
+  "JKR": {
+    "url": "https://jkr.marammuralidhar3234.workers.dev/rest/",
+    "encKey": "Fr4R+Qu6i1p+hdfcmDrm4bywTHY/b863GlNnuA9p9uo3x5nnahlJqvssPOJRt1BcDXLw34a2rIrcT81DmMe7xQ==",
+    "name": "Jakkur",
+    "whatsapp_number": ["919743823134", "917892256106"]
+  },
+  "HRLR": {
+    "url": "https://hrlr.marammuralidhar3234.workers.dev/rest/",
+    "encKey": "bKuxUJfSNeOtp67m3v4WC39P+xPn/7a5QJNCYF1ogTW0OXSgOaxpNb+jvSBQq+93kZGHU3Bj87QFi9FAk7No+g==",
+    "name": "Haralur",
+    "whatsapp_number": ["918073959696", "919663233832"]
+  }
 };
 
 //  "HRLR": { "url": "https://ylghrlr-1795.restdb.io/rest/",
@@ -79,11 +91,8 @@ employee_name_map = {
   'Toheed': "Sameer"
 };
 
-shops_map = {"JKR": "Jakkur", "TNS": "Thanisandra", "HRLR": "Haralur"};
-ylg_shops = ["HRLR"];
-
 function is_ylg(){
-  return ylg_shops.includes(shopSelect.value);
+  return ["HRLR"].includes(shopSelect.value);
 }
 
 function get_emp_name(emp_name){
@@ -328,7 +337,7 @@ function login() {
       if (data == null) {return};
       console.log(data);
       loginDiv.style.display = "none";
-      shopName.textContent = "Reports for - " + shops_map[shop];
+      shopName.textContent = "Reports for - " + shopConfig[shop].name;
 
       // Simple render for debugging
       console.log("✅ Data:", data);
@@ -830,6 +839,12 @@ function goto_selection(reportType, search_value){
 }
 
 function send_whatsapp(text, phone_num=null){
+//  fetch("http://localhost:3000/send", {
+//    method: "POST",
+//    headers: {"Content-Type": "application/json"},
+//    body: JSON.stringify({"number":phone_num, "message":text}) // full record with updated field
+//  })
+//  .then(response => alert(response.json()))
   wa_link = "https://api.whatsapp.com/send/?";
     if(phone_num != null){
       wa_link = wa_link + "phone=91" + phone_num + "&";
@@ -1095,7 +1110,7 @@ function send_update(nrs_only=false, is_update=true, client_count=0, appointment
     return;
   }
   summary =  "Date: *" + today.Date + "*\n";
-  summary += "Salon: *" + shops_map[shopSelect.value] + "*\n\n";
+  summary += "Salon: *" + shopConfig[shopSelect.value].name + "*\n\n";
   summary += "Sales: " + today.NetSale.toFixed(2) + "\n";
   summary += "Bills: " + today.Bills + "\n";
   summary += "ABV: " + today.ABV.toFixed(2) + "\n";
@@ -1161,6 +1176,7 @@ function send_update(nrs_only=false, is_update=true, client_count=0, appointment
     update_str = is_update ? "Update" : "Closing";
     summary += update_str + " Time: " + get_current_time()+ "\n";
   }
+  //  shopConfig[shopSelect.value].whatsapp_number.forEach(phone_num => { send_whatsapp(summary, phone_num); });
   send_whatsapp(summary);
 }
 
